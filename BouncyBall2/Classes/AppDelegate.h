66f1f0951ab8b53a5b0f161613e0e504d70e8f39
Eric Muxagata <ericgmconrado@gmail.com>
2014-02-21 15:24:24 -0500

    foundation.

---
+++ BouncyBall2/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  BouncyBall2
+//
+//  Created by Eric Muxagata on 2/21/14.
+//  Copyright Eric Muxagata 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ BouncyBall2/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BouncyBall2
+//
+//  Created by Eric Muxagata on 2/21/14.
+//  Copyright Eric Muxagata 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
